5d791295033139168cca5cd6f249fc719501b025
Gilgamesh-hoang <vophihoang252003@gmail.com>
2024-04-23 16:00:48 +0700

    first

---
+++ src/main/java/com/shoe/shoeproject/ShoeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.shoe.shoeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoeProjectApplication.class, args);
+    }
+
+}
